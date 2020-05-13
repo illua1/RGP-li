a5b72304a1fa20d6ce601c66f43b635716aa65e3
illua1 <38986703+illua1@users.noreply.github.com>
2020-05-13 22:39:16 +0300

    Add files via upload

--- RPG libraries by mod - test 1-1-1/world test 1.bat
+++ RPG libraries by mod - test 1-1-1/world test 1.bat
@@ -1,3 +1,3 @@
-go build "F:\Golang Builder\world test 1.go"
-go run "F:\Golang Builder\world test 1.go"
+go build "F:\Golang Builder\RPG libraries by mod - test 1-1-1\world test 1.go"
+go run "F:\Golang Builder\RPG libraries by mod - test 1-1-1\world test 1.go"
 pause
